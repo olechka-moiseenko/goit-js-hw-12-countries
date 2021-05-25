44b3d7f051d00cacff9b75bec52dceda0e68f01f
John Doe <olechka.moiseenko@gmail.com>
2021-05-25 15:46:22 +0300

    Changes in css

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
 import "./sass/style.scss";
 import { fetchCountries } from "./fetchCountries.js";
-import debounce from 'lodash.debounce';
+import debounce from "lodash.debounce";
 import countriesCardTpl from "./templates/one-country.hbs";
 import countriesList from "./templates/list-countries.hbs";
-import { tooManyItems, noItemsQualified, closeAllMessages } from './pnotify.js';
+import { tooManyItems, noItemsQualified, closeAllMessages } from "./pnotify.js";
 
 const refs = {
   input: document.querySelector("#country-name"),
   result: document.querySelector("#result"),
-}
+};
 
 refs.input.addEventListener("input", debounce(onInputSearch, 500));
 
-function onInputSearch () {
-  refs.result.innerHTML = '';
+function onInputSearch() {
+  refs.result.innerHTML = "";
   closeAllMessages();
   const name = refs.input.value;
-  fetchCountries(name) 
-   .then(show)
-   .catch((error) => noItemsQualified());
+  if (name) {
+    fetchCountries(name)
+      .then(show)
+      .catch((error) => noItemsQualified());
+  }
 }
 
-
 function show(countries) {
- if (countries.length === 1) {
+  if (countries.length === 1) {
     refs.result.innerHTML = countriesCardTpl(countries[0]);
-  }
-  else if(countries.length < 10)
-  {
+  } else if (countries.length < 10) {
     refs.result.innerHTML = countriesList(countries);
-  }
-  else
-  {
+  } else {
     tooManyItems();
   }
 }
